ee386569d655068085d288f01f730dcd4ae08533
andrew <90sidort@gmail.com>
2021-08-13 17:52:48 +0200

    add search and pagination:

--- src/components/Skills/SkillsList.js
+++ src/components/Skills/SkillsList.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useHistory, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
   Button,
   List,
   ListItem,
   ListItemText,
+  TablePagination,
+  TextField,
   Typography,
 } from "@material-ui/core";
 
@@ -16,10 +18,12 @@ import { getSkillsAction } from "../../actions/skill";
 import { getParameters } from "../../utils/parameter";
 
 const SkillsList = () => {
+  const history = useHistory();
   const cid = parseInt(useLocation().search.split("categoryId=")[1]);
   const url = useLocation().search;
   const classes = useStylesList();
   const dispatch = useDispatch();
+  const [searchObject, setSearchObject] = useState(getParameters(url));
   const { categories: categories } = useSelector((state) => state.categories);
   const category = categories.find((cat) => cat.id === cid);
   const skillsState = useSelector((state) => state.skills);
@@ -27,17 +31,53 @@ const SkillsList = () => {
     user: { token, admin },
   } = useSelector((state) => state.authentication);
   const {
-    skills: { data },
+    skills: { data, last, limit, total },
     loading,
     error,
   } = skillsState;
+
+  const onSearchChange = (search) => {
+    const {
+      target: { value },
+    } = search;
+    const searchValue = value.trim().length === 0 ? "" : value;
+    setSearchObject({ name: searchValue });
+    history.push(`?name=${searchValue}&currentPage=${1}&limit=${limit}`);
+  };
+
+  const handleChangePage = (e, newPage) => {
+    history.push(
+      `?currentPage=${newPage + 1}&limit=${limit}&name=${
+        searchObject.name ? searchObject.name : ""
+      }`
+    );
+  };
+  const handleChangeRowsPerPage = (e) =>
+    history.push(
+      `?currentPage=${1}&limit=${e.target.value}&name=${
+        searchObject.name ? searchObject.name : ""
+      }`
+    );
+
   useEffect(() => {
-    const search = getParameters(url);
-    dispatch(getSkillsAction(token, search));
-  }, [dispatch]);
+    const timer = setTimeout(() => {
+      const search = getParameters(url);
+      dispatch(getSkillsAction(token, search));
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [dispatch, url]);
 
   return (
     <React.Fragment>
+      {!cid && (
+        <TextField
+          id="search"
+          label="Search"
+          variant="outlined"
+          onChange={onSearchChange}
+          value={searchObject.name}
+        />
+      )}
       {category && (
         <Typography variant="h3" gutterBottom>
           {`${category.name} skills`}
@@ -50,6 +90,16 @@ const SkillsList = () => {
         <AppModal open={true} title={"Error"} message={error.message} />
       )}
       {loading && <Spinner />}
+      {data && (
+        <TablePagination
+          component="div"
+          count={total}
+          page={Math.ceil(last / limit) - 1}
+          onPageChange={handleChangePage}
+          rowsPerPage={limit}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      )}
       {data && !error && !loading && (
         <List className={classes.root}>
           {data.length > 0 ? (
